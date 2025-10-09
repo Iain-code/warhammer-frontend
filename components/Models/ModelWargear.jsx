f9198bd34c2502af9b90c26111a37d6b4889df5a
iain-code <iainvirgo1010@live.co.uk>
2025-10-09 14:53:53 +0300

    style

--- components/Models/ModelWargear.jsx
+++ components/Models/ModelWargear.jsx
@@ -48,18 +48,18 @@ const ModelWargear = () => {
 
   return (
     <div>
-      <div className='flex flex-col justify-center text-center'>
+      <div className='flex flex-col justify-center text-center border border-white'>
         <h1 className='text-xl my-2 text-white'>Select a weapon profile</h1>
       </div>
       <div>
         {atkWargear && atkWargear.length > 0 &&
-        <div className='flex justify-center text-white'>
+        <div className='flex justify-center text-white border border-red'>
           <ul className="
             grid w-full max-w-5xl gap-3
             grid-cols-1 sm:grid-cols-3 lg:grid-cols-5 xl:grid-cols-6
           ">
             {atkWargear.map(item => (
-              <li key={item.id} className="aspect-[2/1]"> 
+              <li key={item.id} className="aspect-[2/1] border border-blue"> 
                 <button
                   onClick={() => chooseWargear(item)}
                   className="

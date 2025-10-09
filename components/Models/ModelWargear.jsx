80eca997b9dee1eb6f62b03ab4225ec4a695a712
iain-code <iainvirgo1010@live.co.uk>
2025-10-09 12:40:47 +0300

    style

--- components/Models/ModelWargear.jsx
+++ components/Models/ModelWargear.jsx
@@ -54,7 +54,7 @@ const ModelWargear = () => {
       <div>
         {atkWargear && atkWargear.length > 0 &&
         <div className='flex justify-center text-white'>
-          <ul className="flex flex-col items-center gap-3 w-3/4 lg:flex-row lg:flex-wrap lg:justify-center lg:stretch-items">
+          <ul className="flex flex-col items-center gap-3 w-3/4 lg:flex-row lg:flex-wrap lg:justify-center">
             {atkWargear.map(item =>
               <li key={item.id} className="flex flex-1 w-3/4 lg:w-1/5">
                 <button
